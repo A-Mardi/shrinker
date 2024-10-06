6276595d8ae6669507f41df8b639a9d5141318e9
Ahmed Ibrahim <ahmed.mardi09@gmail.com>
2024-10-06 17:11:33 -0400

    Add an input handler header file

---
+++ shrinker/input_handler.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#ifndef INPUT_HANDLER_H
+#define INPUT_HANDLER_H
+
+#include <string>
+
+void handleInput(int argc, char* argv[], std::string& inputFilePath, std::string& compressedFilePath, std::string& decompressedFilePath);
+
+#endif // INPUT_HANDLER_H
